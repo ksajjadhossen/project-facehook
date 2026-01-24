7f842b0a28f06e2ea918c4001c64da771f179e0d
ksajjadhossen <ksajjadhossen07@gmail.com>
2026-01-24 17:29:53 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import {Routes, Route} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import NotFoundPage from './pages/NotFoundPage';
-import ProfilePage from './pages/ProfilePage';
-import RegistrationPage from './pages/RegistrationPage';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegistrationPage from "./pages/RegistrationPage";
 
 function App() {
-
   return (
     <>
       <Routes>
@@ -18,7 +17,7 @@ function App() {
         <Route element={<NotFoundPage />} path="*" />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     container: {
       center: true,
@@ -22,5 +19,4 @@ export default {
     },
   },
   plugins: [require("@tailwindcss/typography")],
-}
-
+};
